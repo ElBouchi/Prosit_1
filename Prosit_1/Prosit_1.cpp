06d60611cad328502f59e29c0ca5d7b0941434bb
El Bouchi <deadalex64@gmail.com>
2020-11-02 22:24:35 +0100

    Calcul

--- Prosit_1/Prosit_1.cpp
+++ Prosit_1/Prosit_1.cpp
@@ -1,20 +1,58 @@
-// Prosit_1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+double Xa, Xb, Ya, Yb;
+
+class Euclidienne {
+public:
+    double Xa, Xb, Ya, Yb;
+    void afficher() {
+        double Distance = sqrt(pow(Xb - Xa, 2) + pow(Yb - Ya, 2));
+        cout << "la distance Euclidienne entre ces deux points est de : " << Distance << endl;
+    }
+};
+
+class Tchebychev {
+public:
+    double Xa, Xb, Ya, Yb;
+    void afficher() {
+        double Distance = fmax((abs(Xb - Xa)), (abs(Yb - Ya)));
+        cout << "la distance Tchebychev entre ces deux points est de : " << Distance << endl;
+    }
+};
+
+class Manhattan {
+public:
+    double Xa, Xb, Ya, Yb;
+    void afficher() {
+        double Distance = abs(Xb - Xa) + abs(Yb - Ya);
+        cout << "la distance Manhattan entre ces deux points est de : " << Distance << endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Euclidienne Eucl;
+    Tchebychev Tcheb;
+    Manhattan Manh;
+
+    cout << "Entrez les coordoonees du point A : \n";
+    cin >> Xa >> Ya;
+    cout << "Les coordoonees du point A sont : \n";
+    cout << Xa << "," << Ya << "\n";
+    cout << "Entrez les coordonnees du point B : \n";
+    cin >> Xb >> Yb;
+    cout << "Les coordoonees du point B sont : \n";
+    cout << Xb << "," << Yb << "\n";
+
+    Eucl.Xa = Xa, Eucl.Xb = Xb, Eucl.Ya = Ya, Eucl.Yb = Yb;
+    Tcheb.Xa = Xa, Tcheb.Xb = Xb, Tcheb.Ya = Ya, Tcheb.Yb = Yb;
+    Manh.Xa = Xa, Manh.Xb = Xb, Manh.Ya = Ya, Manh.Yb = Yb;
+
+    Eucl.afficher();
+    Tcheb.afficher();
+    Manh.afficher();
+
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
